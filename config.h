f29bcdd5f4d52f0f7e331ce1e3ab93b57643bfd2
newUser161 <n4k4.1184@gmail.com>
2023-10-23 20:39:18 -0300

    parametros por defecto

--- config.h
+++ config.h
@@ -7,25 +7,25 @@
 struct ACOArgs : public argparse::Args
 {
     std::string &nombre_instancia = arg("Nombre de la instancia").set_default("Formato5x5.txt");
-    int &metodo = kwarg("metodo", "Metodo de resolucion").set_default(1);
+    int &metodo = kwarg("metodo", "Metodo de resolucion").set_default(0);
     float &alfa = kwarg("alfa", "Parámetro alfa").set_default(1.0f);
     float &beta = kwarg("beta", "Parámetro beta").set_default(2.0f);
-    float &rho = kwarg("rho", "Parámetro rho, asociado a la evaporación de feromonas").set_default(0.2f);
+    float &rho = kwarg("rho", "Parámetro rho, asociado a la evaporación de feromonas").set_default(0.5f);
     float &rho_secundario = kwarg("rho-sec", "Parámetro rho, asociado a la evaporación de feromonas").set_default(0.8f);
     float &rho_salida = kwarg("rho-salida", "Parámetro rho de salida, asociado a la evaporación de feromonas").set_default(0.1f);
     float &tau = kwarg("tau-as", "Parámetro tau, asociado a las feromonas iniciales").set_default(1.0f);
     //int &iteraciones_max = kwarg("iter-max", "Cantidad de iteraciones máximas").set_default(1000); // usar numero de evaluaciones o de iteraciones, pero no ambos
     int &evaluaciones_maximas = kwarg("eval-max", "Cantidad de evaluaciones máximas").set_default(10000);// 
-    double &umbral_inferior = kwarg("umbral-inf", "Umbral inferior para las feromonas").set_default(1.7e-100);
+    double &umbral_inferior = kwarg("umbral-inf", "Umbral inferior para las feromonas").set_default(1.7e-10);
     int &num_hormigas = kwarg("num-hormigas", "Número de hormigas").set_default(10);
     int &epocas = kwarg("epocas", "Épocas").set_default(3);
     double &semilla = kwarg("semilla", "semilla de generacion aleatoria").set_default(161);
     bool &usaMatrizSecundaria = flag("matriz-secundaria", "Flag que permite o no el uso de la matriz de salida").set_default(true);
 
     // MinMax
-    double &umbral_superior = kwarg("umbral-sup", "Umbral superior para las feromonas").set_default(10000);
+    double &umbral_superior = kwarg("umbral-sup", "Umbral superior para las feromonas").set_default(161161.161);
     int &umbral_sin_mejora_limite = kwarg("umbral-sin-mejora", "Cantidad de iteraciones sin mejora para la actualización de feromonas").set_default(10);
-    int &a = kwarg("a-mm", "Parámetro a, asociado a la actualización de feromonas").set_default(50);
+    int &a = kwarg("a-mm", "Parámetro a, asociado a la actualización de feromonas").set_default(13);
 
     // ACS
     double &q_0 = kwarg("q0", "Parámetro q0").set_default(0.5); // umbral de probabilidad ACS
