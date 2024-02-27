94e64ad4b7f1abf62cfe822b2aef224549753d1b
Alonso Robledo <n4k4.1184@gmail.com>
2024-02-26 22:31:42 -0300

    Ejecucion multiple

--- EjecutarMultiple.ps1
+++ EjecutarMultiple.ps1
@@ -1,2 +1,199 @@
+for ($i=0; $i -lt 10; $i++) {
+    ./main Formato5x5.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa A1"
 
-./Main.exe CasoRealMediano.txt --num-hormigas 10 --epocas 3 --iter-max 1000 --metodo 0
+for ($i=0; $i -lt 10; $i++) {
+    ./main Formato5x5.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3 --matriz-secundaria  --usar-iteraciones    --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa A1"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main Formato5x5.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3  --beta0 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa A1"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main Formato5x5.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3  --usar-limitador --valor-limitador 2 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa A1"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main Formato5x5.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa A1"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main Formato5x5.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3 --matriz-secundaria  --usar-iteraciones    --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa A1"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main Formato5x5.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3  --beta0 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa A1"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main Formato5x5.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3  --usar-limitador --valor-limitador 2 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa A1"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main CasoRealChiquito.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa CRP"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main CasoRealChiquito.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3 --matriz-secundaria  --usar-iteraciones    --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa CRP"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main CasoRealChiquito.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3  --beta0 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa CRP"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main CasoRealChiquito.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3  --usar-limitador --valor-limitador 2 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa CRP"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main CasoRealChiquito.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa CRP"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main CasoRealChiquito.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3 --matriz-secundaria  --usar-iteraciones    --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa CRP"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main CasoRealChiquito.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3  --beta0 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa CRP"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main CasoRealChiquito.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3  --usar-limitador --valor-limitador 2 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa CRP"
+
+
+
+
+
+
+
+
+
+
+
+
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealMediano.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRM"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealMediano.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3 --matriz-secundaria  --usar-iteraciones    --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRM"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealMediano.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3  --beta0 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRM"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealMediano.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3  --usar-limitador --valor-limitador 2 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRM"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealMediano.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRM"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealMediano.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3 --matriz-secundaria  --usar-iteraciones    --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRM"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealMediano.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3  --beta0 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRM"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealMediano.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3  --usar-limitador --valor-limitador 2 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRM"
+
+
+
+
+
+
+
+
+
+
+
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealGrande.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3 --matriz-secundaria  --usar-iteraciones    --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRG"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealGrande.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3  --beta0 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRG"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealGrande.txt --metodo 0 --iter-max 1000 --num-hormigas 10 --epocas 3  --usar-limitador --valor-limitador 2 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRG"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealGrande.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRG"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealGrande.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3 --matriz-secundaria  --usar-iteraciones    --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRG"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealGrande.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3  --beta0 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRG"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealGrande.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3  --usar-limitador --valor-limitador 2 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+"Ejecucion Completa  CRG"
+
+for ($i=0; $i -lt 10; $i++) {
+    ./main  CasoRealGrande.txt --metodo 1 --iter-max 1000 --num-hormigas 10 --epocas 3  --beta0 --matriz-secundaria --usar-limitador --valor-limitador 2 --usar-iteraciones   --crear-directorios-solucion --bdconn
+}
+
+
+
+
+
